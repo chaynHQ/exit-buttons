05a68b117c5fd80b9d6148f32773033524695820
Hera Hussain <hussain.hera@gmail.com>
2018-08-26 17:21:26 +0100

    Add files via upload

---
+++ function.php
@@ -0,0 +1,18 @@
+/* Add to the bottom of the page */
+
+	function addHideSiteButton() {
+?>
+<div id="hide-site">
+    <button onclick="hideSite()">Leave this site</button>
+</div>
+
+<script>
+    function hideSite() {
+        window.open("https://en.wikipedia.org/wiki/Main_Page", "_newtab");
+        location.replace("https://www.google.co.uk/search?tbm=isch&sa=1&ei=esSCW4LPH4WugQaZsbqoDw&q=cute+baby+animal+memes&oq=cute+baby+animal+memes&gs_l=")
+    }
+</script>
+<?php
+	}
+	add_action('theme_after_body_tag_start','addHideSiteButton')
+?>
